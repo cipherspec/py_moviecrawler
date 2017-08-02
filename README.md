7c8238be5ee94c721419e3865861b18eeefd8e92
twoDarkMessiah <twoDarkMessiah@gmail.com>
2017-08-02 02:03:08 +0200

    update 0.4.0
    added option --scrap-all

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple multithreaded python movie crawler
 Disclaimer: I'm not responsable for the use of this software. Check if downloading of movies is in accordance with the laws of your country!
 
 Description:
-Search for filecrypt links (ul, so) for movies. Filtered by 1080p.
+Search for filecrypt links (ul, so) for movies from W-H. The best available releases are picked. 
 
 Usage: main.py [-h] [-o OUTPUT] [-m MISSING] [-t THREADS] file
 
--- py_moviecrawler.py
+++ py_moviecrawler.py
@@ -1,6 +1,6 @@
 #Author: twoDarkMessiah (twoDarkMessiah@gmail.com)
-#Version: 0.3.0
-#Date: 2017-07-30 21:45
+#Version: 0.4.0
+#Date: 2017-08-02 01:48
 #License: GPL 3
 
 import urllib
@@ -12,7 +12,6 @@ from urllib import parse
 import os
 import lxml.html
 import xml.etree.ElementTree as xml
-from pip._vendor import requests
 
 #set allowed OCHs
 allowed_hoster = ["Uploaded.net", "Share-online.biz"]
@@ -23,6 +22,50 @@ plex_url = "http://plex.example.com:32401"
 plex_auth_token = "<YOUR-PLEX-TOKEN>"
 plex_library_id = 2 #Your Movie Library
 plex_use = False #True, to compare movielist with plex
+
+def findAllMovies(page_number):
+    results = []
+    try:
+        resource = urllib.request.urlopen("https://warez-heaven.ws/hauptkategorie/Full-HD/page/" + str(page_number))
+        content = resource.read().decode(resource.headers.get_content_charset())
+        page = lxml.html.fromstring(content)
+        elms = page.xpath("//div[@class='flag__body']")
+        for elm in elms:
+            title = ""
+            release = ""
+            size = 0.0
+            links = []
+            info_link = None
+            for child in elm.getchildren():
+                if (child.tag is not None):
+                    if (child.tag == "h4" and child.text is not None):
+                        release = child.text
+                        tvshow = len(re.findall('S\d{2}', release))
+                        if not ("1080p" in release and "XXX" not in release and '3D' not in release and tvshow == 0):
+                            release = ""
+                    if (child.tag == "span" and child.text is not None):
+                        sizes = re.findall("\d{1,2}\.{1}\d{2}\sGB", child.text)
+                        if (len(sizes) > 0):
+                            size = float(sizes[0].replace(" GB", ""))
+                    if (child.tag == "h3" and child.text is not None):
+                        title = child.text
+                    if (child.tag == "a" and child.get("title") != None and child.get(
+                            "title") in allowed_hoster and child.get("href") != None):
+                        links.append((child.get("title"), child.get("href")))
+                    if (child.tag == "a" and child.get("title") == "Warezkorb" and child.get("href") != None):
+                        tmp_info_link = child.get("href")
+                        re_result = re.findall("\d{5,6}", tmp_info_link)
+                        if (len(re_result) > 0):
+                            info_link = "https://warez-heaven.ws/" + str(re_result[0]) + "/"
+            if (info_link != None):
+                info_link = info_link + release
+                if (title != "" and release != "" and size != 0.0 and len(links) > 0 and info_link != None):
+                    results.append((title, release, size, info_link, links))
+    except:
+        print ("Error on Page:" + str(page_number))
+    print (str(len(results)) + " results on page " + str(page_number))
+    return results
+
 
 def findLink(movie):
     print("Searching for: " + movie.strip())
@@ -111,7 +154,6 @@ def moviedbsearch(title):
     print (res.read())
 
 def main():
-
     movies = []
     movies_found = []
     movies_missing = []
@@ -120,65 +162,93 @@ def main():
     file_missing = None
 
     parser = argparse.ArgumentParser(prog='main.py')
-    parser.add_argument("file", help='specifay source file for movies')
+    parser.add_argument("-i", "--input", help='specifay source file for movies')
     parser.add_argument("-o", "--output", help='file to save filecrypt links')
     parser.add_argument("-m", "--missing", help='file to save missing (failed) movies')
     parser.add_argument("-t", "--threads", help="how much threads should be used (default: 4)", default=4)
+    parser.add_argument("-a", "--scrap-all", help="scrap all 1080p movies from W-H", dest="all", action='store_true')
+
     args = parser.parse_args()
 
 
-    if(not (os.path.isfile(args.file))):
-        print("File '" + args.file + "' does not exits. Abort!" )
-        return
+    if(not args.all):
+        if(args.input == None):
+            print("You must either specify an input file, or scrap all with --scrap-all")
+            return
+        if(not (os.path.isfile(args.input))):
+            print("File '" + args.input + "' does not exits. Abort!" )
+            return
 
     plex_movies = []
     if(plex_use):
         plex_movies = loadPlexMovies()
 
+
     skip_plex = 0
-    for line in open(args.file, 'r', encoding="utf-8"):
-        if(len(line.strip()) >= 4):
-            if (line.strip() in plex_movies):
-                skip_plex = skip_plex + 1
-                print(line.strip())
+    if(not args.all):
+        for line in open(args.input, 'r', encoding="utf-8"):
+            if(len(line.strip()) >= 4):
+                if (line.strip() in plex_movies):
+                    skip_plex = skip_plex + 1
+                    print(line.strip())
+                else:
+                    movies.append(line.strip())
+
+        print (str(len(movies)) + " movies in list")
+        if(plex_use):
+            print (str(skip_plex) + " movies skipped, they are already in Plex")
+        print ("Using " + args.threads + " threads")
+        if int(args.threads) > 8:
+            print("Warning: more than 8 threads can lead to random crashes")
+
+        pool = mp.Pool(processes=int(args.threads), maxtasksperchild=1)
+        results = pool.map(findLink, movies)
+        pool.close()
+
+        if (len(movies) > 0):
+            if (not (args.output == None)):
+                file_out = open(args.output, 'w')
+
+        fh_missing = None
+        if (args.missing != None):
+            fh_missing = open(args.missing, 'w', encoding="utf-8")
+
+        for result in results:
+            if(result[0] != None):
+                movies_found.append(result[1])
+                if(file_out != None):
+                    file_out.write(result[0].strip() + "\n")
             else:
-                movies.append(line.strip())
+                movies_missing.append(result[1])
+                if(args.missing != fh_missing != None):
+                    fh_missing.write(result[1].strip() + "\n")
+        if(file_out != None):
+            file_out.close()
+        if(file_missing != None):
+            file_missing.close()
 
-    print (str(len(movies)) + " movies in list")
-    if(plex_use):
-        print (str(skip_plex) + " movies skipped, they are already in Plex")
-    print ("Using " + args.threads + " threads")
-    if int(args.threads) > 8:
-        print("Warning: more than 8 threads can lead to random crashes")
-
-    pool = mp.Pool(processes=int(args.threads), maxtasksperchild=1)
-    results = pool.map(findLink, movies)
-    pool.close()
-
-    if (len(movies) > 0):
+        print("Links to " + str(len(movies_found)) + " from " + str(len(movies)) + " where found!")
+    else:
+        file_out = None
         if (not (args.output == None)):
             file_out = open(args.output, 'w')
-
-    fh_missing = None
-    if (args.missing != None):
-        fh_missing = open(args.missing, 'w', encoding="utf-8")
-
-    for result in results:
-        if(result[0] != None):
-            movies_found.append(result[1])
-            if(file_out != None):
-                file_out.write(result[0].strip() + "\n")
-        else:
-            movies_missing.append(result[1])
-            if(args.missing != fh_missing != None):
-                fh_missing.write(result[1].strip() + "\n")
-
-    if(file_out != None):
-        file_out.close()
-    if(file_missing != None):
-        file_missing.close()
-
-    print("Links to " + str(len(movies_found)) + " from " + str(len(movies)) + " where found!")
-
+        base_url = "https://warez-heaven.ws/hauptkategorie/Full-HD"
+        resource = urllib.request.urlopen(base_url)
+        content = resource.read().decode(resource.headers.get_content_charset())
+        site_count = int(re.findall("Seite\s\d{1}\svon\s\d{1,5}", content)[0].replace("Seite 1 von ", ""))
+        print("Found " + str(site_count) + " pages to crawl, let's do the work :)")
+        pool = mp.Pool(processes=int(args.threads), maxtasksperchild=1)
+        results = pool.map(findAllMovies, range(1, site_count))
+        movie_list = []
+        pool.close()
+        for result_set in results:
+            for entry in result_set:
+                if entry[0] not in movie_list:
+                    if(len(entry[4]) > 0):
+                        movie_list.append(entry[0])
+                        file_out.write(entry[4][0][1] + "\n")
+                        print("new link for: " + entry[1])
+        if(file_out != None):
+            file_out.close()
 if __name__ == "__main__":
     main()
